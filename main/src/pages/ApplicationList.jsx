0fe7b5006bc62e8920e66a6efa24a2d95bc112bb
Bishal Giri <bishal0922@gmail.com>
2023-05-14 18:20:19 -0500

    added <table>

--- main/src/pages/ApplicationList.jsx
+++ main/src/pages/ApplicationList.jsx
@@ -1,11 +1,32 @@
 import Application from "./Application";
+import './styles/table.css'
 
 const ApplicationList = ({applications}) => {
     return(
         <div>
+            <table className="table-main">
+            <thead className="table-head">
+                <tr>
+                    <th className="company-name-header">Company Name</th>   
+                    <th className="status-header">Status</th>
+                    <th className="date-header">Date</th>
+                </tr>
+            </thead>
+
+            <tbody className="table-body">
+            {/* <tr> as table row and <td> as table data 
+                <tr>
+                    <td>Google</td>
+                    <td>Applied</td>
+                    <td>10/10/2021</td>
+                </tr>
+             */}
             {applications.map ( (app) => {
                 return <Application key= {app.id} app = {app}/>
             })}
+            </tbody>
+
+            </table>
         </div>
     )
 }
