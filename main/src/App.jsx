7c423b604d3936d1ea72184bcac4d05501a26adf
Bishal Giri <bishal0922@gmail.com>
2023-06-09 19:11:08 -0700

    refactor App.jsx with a new Component

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,94 +1,14 @@
-import { useState } from "react";
-import { useRef } from "react"
-import ApplicationList from "./pages/ApplicationList";
 import Navbar from "./pages/Navbar";
-import {v4 as uuidv4} from 'uuid';
-import { useEffect } from "react";
+import AppKeep from "./pages/AppKeep";
 import './App.css'
-import ExportCSV from "./pages/ExportCSV";
-import ExportJSON from "./pages/ExportJSON";
-
-const LOCAL_STORAGE_KEY = "AppKeep"
 
 const App = () => {
-  const [applications, setApplications] = useState([
-  ]);
-  const companyName = useRef();
-
-  //useEffect when the program runs first time
-  useEffect( () => {
-    const storedApplications = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-
-    if (storedApplications){
-      setApplications(storedApplications);
-    }
-  }, [])
-
-  //useEffect changes when there are changes to todos
-  useEffect( () => {
-    //store locally
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(applications))
-  }, [applications])
-
-  //handle key down
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter'){
-      handleAddApplication();
-    }
-  }
-
-  const handleAddApplication = () => {
-    //get the company name from the input field
-    const name = companyName.current.value;
-
-    //verify it's not empty
-    if (name === ''){
-      return 
-    }
-
-    const newApplication = {
-      id: uuidv4(), //create a unique id
-      companyName:  name,
-      status: "Applied",
-      date: new Date().toLocaleDateString('en-US', {month: "long", day: "numeric"})
-    }
-
-    //update state with new application
-    setApplications(prevApplications => {
-      return [...prevApplications, newApplication]
-    })
-
-    //clear the input field
-    companyName.current.value = "";
-  }
-
-
 
   return (
     <div className="App">
       <Navbar />
-      <div className="application">
-        {/* Section to Add a new Application */}
-        <div className="application-input">
-          <input type="text" ref={companyName} placeholder="Company Name (Ex Google...)" onKeyDown={handleKeyDown}/>
-          <button onClick={handleAddApplication}> Add Application </button>
-          {/* add a section to show number of active application */}
-        </div>
-
-        <div className="application-list">
-          
-          {/* Section to Display the list of Applications */}
-          <ApplicationList applications={applications} setApplications={setApplications}/>
-        </div>
-        <div className="export">
-          <ExportJSON applications={applications}/>
-        </div>
-
-        <div className="export-csv">
-          <ExportCSV applications={applications}/>
-        </div>
-
-      </div>
+      <AppKeep/>
+      
     </div>
   );
 }
---
+++ main/src/pages/AppKeep.jsx
@@ -0,0 +1,94 @@
+import { React, useState, useRef, useEffect } from "react";
+import {v4 as uuidv4} from 'uuid';
+import '../App.css'
+
+import ApplicationList from "./ApplicationList";
+import ExportCSV from "./ExportCSV";
+import ExportJSON from "./ExportJSON";
+
+
+const LOCAL_STORAGE_KEY = "AppKeep"
+
+const AppKeep = () => {
+
+    const [applications, setApplications] = useState([
+    ]);
+    const companyName = useRef();
+  
+    //useEffect when the program runs first time
+    useEffect( () => {
+      const storedApplications = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  
+      if (storedApplications){
+        setApplications(storedApplications);
+      }
+    }, [])
+  
+    //useEffect changes when there are changes to todos
+    useEffect( () => {
+      //store locally
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(applications))
+    }, [applications])
+  
+    //handle key down
+    const handleKeyDown = (e) => {
+      if (e.key === 'Enter'){
+        handleAddApplication();
+      }
+    }
+  
+    const handleAddApplication = () => {
+      //get the company name from the input field
+      const name = companyName.current.value;
+  
+      //verify it's not empty
+      if (name === ''){
+        return 
+      }
+  
+      const newApplication = {
+        id: uuidv4(), //create a unique id
+        companyName:  name,
+        status: "Applied",
+        date: new Date().toLocaleDateString('en-US', {month: "long", day: "numeric"})
+      }
+  
+      //update state with new application
+      setApplications(prevApplications => {
+        return [...prevApplications, newApplication]
+      })
+  
+      //clear the input field
+      companyName.current.value = "";
+    }
+
+
+  return (
+    <div>
+        <div className="application">
+        {/* Section to Add a new Application */}
+        <div className="application-input">
+          <input type="text" ref={companyName} placeholder="Company Name (Ex Google...)" onKeyDown={handleKeyDown}/>
+          <button onClick={handleAddApplication}> Add Application </button>
+          {/* add a section to show number of active application */}
+        </div>
+
+        <div className="application-list">
+          
+          {/* Section to Display the list of Applications */}
+          <ApplicationList applications={applications} setApplications={setApplications}/>
+        </div>
+        <div className="export">
+          <ExportJSON applications={applications}/>
+        </div>
+
+        <div className="export-csv">
+          <ExportCSV applications={applications}/>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default AppKeep
