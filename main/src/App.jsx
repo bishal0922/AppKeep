36da52859f119e60e941e9f8a8677c77eca69813
Bishal Giri <bishal0922@gmail.com>
2023-06-11 20:08:52 -0700

    added react-router-dom implementation (working)

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,14 +1,18 @@
 import Navbar from "./pages/Navbar";
 import AppKeep from "./pages/AppKeep";
+import Login from "./pages/Login";
 import './App.css'
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
 
   return (
-    <div className="App">
+    <div>
       <Navbar />
-      <AppKeep/>
-      
+      <Routes>
+        <Route path="/" element={<AppKeep />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- main/src/pages/Navbar.jsx
+++ main/src/pages/Navbar.jsx
@@ -1,22 +1,25 @@
 import './styles/navbar.css'
 //add image
 import logo from '../assets/logo/android-chrome-512x512.png'
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="logo">
-                <img src={logo} alt="logo" />
-            </div>
-            
-            <div className='AppKeep-title'>
-                <a href="/">AppKeep</a>
-            </div>
+      <nav className="navbar">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
 
-            <ul>
-                <li><a className="links" href="/about">Login</a></li>
-            </ul>
-        </nav>
+        <div className="AppKeep-title">
+          <Link to="/">Appkeep</Link>
+        </div>
+
+        <ul>
+          <li>
+            <Link className="links" to="/Login">Login</Link>
+          </li>
+        </ul>
+      </nav>
     );
 }
 
