5a892a2b609890c3cf8046674cf63885f3df7630
Bishal Giri <bishal0922@gmail.com>
2023-10-04 17:46:26 -0500

    add .env to gitignore, install packages, +config

--- main/vite.config.js
+++ main/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env': env
+  }
+});
