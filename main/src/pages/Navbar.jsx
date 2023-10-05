4559387c62619fd2d67c748dd9aa9737fbaf946a
Bishal Giri <bishal0922@gmail.com>
2023-10-04 21:53:54 -0500

    navbar to reflect auth state

--- main/src/pages/Navbar.jsx
+++ main/src/pages/Navbar.jsx
@@ -1,9 +1,45 @@
 import './styles/navbar.css'
 //add image
 import logo from '../assets/logo/android-chrome-512x512.png'
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {  onAuthStateChanged} from "firebase/auth";
+import { React, useState, useEffect } from "react";
+import { auth } from "../firebase";
 
 const Navbar = () => {
+
+   const [authUser, setAuthUser] = useState(null)
+    
+useEffect(() => {
+  try {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+
+    // Clean up the listener when the component unmounts
+
+  } catch (error) {
+    console.error("Error in useEffect:", error);
+  }
+}, []);
+
+useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setAuthUser(user);
+        } else {
+            setAuthUser(null);
+        }
+    });
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
+}, []);
+
+
     return (
       <nav className="navbar">
         <div className="logo">
@@ -14,13 +50,23 @@ const Navbar = () => {
           <Link to="/">Appkeep</Link>
         </div>
 
-        <ul>
+
+           <ul>
+          {authUser ? (
+            <li>
+              <Link className="links" to="/Login">SignOut</Link>
+            </li>
+          ) : (
+            <>
+            <li>
+                    <Link className="links" to="/Login">Login</Link>
+              </li>
+             
           <li>
-            <Link className="links" to="/Login">Login</Link>
-          </li>
-          <li>
-            <Link className="links" to="/Register">Register</Link>
-          </li>
+                <Link className="links" to="/Register">Register</Link>
+              </li>
+              </>
+          )} 
         </ul>
       </nav>
     );
